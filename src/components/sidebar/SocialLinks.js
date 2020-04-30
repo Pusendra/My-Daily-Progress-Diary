da2b3822cc67a54be7f73b5eb6fa65d4f0333ad6
Pusendra Chaudhary <cpusendra@gmail.com>
2020-04-30 09:30:06 +0545

    Update SocialLinks.js

--- src/components/sidebar/SocialLinks.js
+++ src/components/sidebar/SocialLinks.js
@@ -13,31 +13,31 @@ const SocialLinks = ({ contacts }) => {
     return (
         <div className="side-social-links float-left mt-3 mb-3">
             <a className="text-secondary p-2"
-                href={contacts.linkedin}>
+                href={contacts.linkedin} target="_blank">
                 <span title="Linked In">
                     <FaLinkedin size={26} style={{ color: "secondary" }} />
                 </span>
             </a>
             <a className="text-secondary p-2"
-                href={contacts.github}>
+                href={contacts.github} target="_blank">
                 <span title="GitHub">
                     <FaGithubSquare size={26} style={{ color: "secondary" }} />
                 </span>
             </a>
             <a className="text-secondary p-2"
-                href={contacts.stackoverflow}>
+                href={contacts.stackoverflow} target="_blank">
                 <span title="Stack Overflow">
                     <FaStackOverflow size={26} style={{ color: "secondary" }} />
                 </span>
             </a>
             <a className="text-secondary p-2"
-                href={contacts.freecodecamp}>
+                href={contacts.freecodecamp} target="_blank">
                 <span title="freeCodeCamp">
                     <FaFreeCodeCamp size={26} style={{ color: "secondary" }} />
                 </span>
             </a>
             <a className="text-secondary p-2"
-                href={contacts.twitter}>
+                href={contacts.twitter} target="_blank">
                 <span title="Twitter">
                     <FaTwitterSquare size={26} style={{ color: "secondary" }} />
                 </span>
